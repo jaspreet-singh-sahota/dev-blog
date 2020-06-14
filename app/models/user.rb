7a8cf7ef1952eb518b3775a077cde8937c284980
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-14 07:44:03 +0530

    Alter devise defaults in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,34 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+          :authentication_keys => [:username]
+  validates :username, uniqueness: true, length: { minimum: 3, maximun: 25 }
+
+  def email_required?
+    false
+  end
+
+  def email_changed?
+    false
+  end
+  
+  # use this instead of email_changed? for Rails = 5.1.x
+  def will_save_change_to_email?
+    false
+  end
+
+  protected
+
+  def password_required?
+    false
+  end
+
+  def after_confirmation_path_for(resource_name, resource)
+    token = resource.send(:set_reset_password_token)
+    edit_password_path(resource, reset_password_token: token)
+  end
+
+  def valid_password?
+      false
+  end
 end
