71c2648acfffe0ed5399ce41776ff1476108db11
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-14 20:30:39 +0530

    Add validations and associations to article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  belongs_to :user
+  validates :title, presence: true, length: { minimun: 3, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 5000 }
 end
