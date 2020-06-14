abd4dd5dd0aa227e633c7cabc638386e42526096
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-14 20:29:06 +0530

    Run article migrations

--- db/migrate/20200614143618_create_articles.rb
+++ db/migrate/20200614143618_create_articles.rb
@@ -1,7 +1,9 @@
 class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
-
+      t.string :title
+      t.text :text
+      t.string :image
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_14_050216) do
+ActiveRecord::Schema.define(version: 2020_06_14_143618) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "text"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", default: "", null: false
