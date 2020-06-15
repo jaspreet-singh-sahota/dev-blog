61e89dcc721d55415319a8e6e74d9a1aabd5a6a8
jaspreet-singh-sahota <jaspreetsinghjassi01@gmail.com>
2020-06-16 02:30:30 +0530

    Add Associations to model

--- app/models/article.rb
+++ app/models/article.rb
@@ -5,4 +5,8 @@ class Article < ApplicationRecord
   validates :title, :image, :categories, presence: true
   validates :title, presence: true, length: { minimun: 3, maximum: 100 }
   validates :text, presence: true, length: { minimum: 10, maximum: 5000 }
+  has_many :votes
+  has_many :voters , throught: :votes
+  has_many :users_vote, through: :votes, source: :user
+  scope :highest_vote, -> { order(votes_count: :desc) }
 end
